9f37dbf2aac4796a7addd29ef336ad4dffa22822
Maryse M. <Maryse.Mongeau@cegepoutaouais.qc.ca>
2025-05-23 14:54:33 -0400

    Début du premier test.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Cours_Get_Post_Formulaires.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            RequetesController rc = new RequetesController();
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.QueryString = new QueryString("?prenom=Dupont");
+
+            rc.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+
+            var r = rc.ReceptionRequeteGet("allo");
+            Assert.NotNull(r);
+            //TODO : à compléter
+        }
+    }
+}
